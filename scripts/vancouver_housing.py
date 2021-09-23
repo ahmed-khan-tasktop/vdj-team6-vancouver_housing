938168d3e83538c8918bd8165dccccd621ad5142
Ahmed_Khan <ahmed.khan@tasktop.com>
2021-09-23 07:44:21 -0700

    made changes to file. deleted some lines.

--- scripts/vancouver_housing.py
+++ scripts/vancouver_housing.py
@@ -1,6 +1,15 @@
+# resources: https://docs.streamlit.io/en/latest/tutorial/create_a_data_explorer_app.html
+
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 st.title('Vancouver housing')
+
+#getting data:
+df = pd.read_csv (r'/Users/ahmed.khan/Documents/personal/vancouver_datajam/property-tax-report.csv',sep=';')
+# print (df)
+
+
+st.write(df)
 
