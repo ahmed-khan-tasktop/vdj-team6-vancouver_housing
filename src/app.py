3ec7a3088d470cf7b260449f1d4974b6ab729403
Faisal Barazi <f.barazi@gmail.com>
2021-09-23 19:53:40 -0700

    Add dash app for server deployment and helper data file.

---
+++ src/app.py
@@ -0,0 +1,50 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import os
+
+from data import non_market_dataframe
+
+app = dash.Dash(__name__)
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = non_market_dataframe()
+
+# Update me. Properly access the required token for mapbox for deployement.
+token = os.environ['MAPBOX_TOKEN']
+
+
+# TODO: Read the token form a text file. Easier
+px.set_mapbox_access_token(token)
+fig = px.scatter_mapbox(
+    df,
+    lat='latitude',
+    lon='longitude',
+    mapbox_style='carto-positron',
+    hover_name='Name',
+    hover_data=['Operator', 'Local Area', 'Total Units'],
+    color='Project Status',
+    color_discrete_sequence=['#5b606d', '#689626', '#c78d00', '#a42726'],
+    size='Total Units',
+    zoom=10
+)
+
+app.layout = html.Div(children=[
+    html.H1(children='Interactive map of Non Market Housing in Vancouver.'),
+
+    html.Div(children='''
+        The map shows locations and information of non market housing
+        between the years 1958 - 2020.\nThe projects are color coded depending
+        on their phase and the scale reflects the capacity of the unit.
+    '''),
+
+    dcc.Graph(
+        id='interactive-map',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ src/data.py
@@ -0,0 +1,34 @@
+"""
+Returns non market dataframe extracted from the data folder,
+cleaned-ish and transformed
+"""
+from pathlib import Path
+import json
+import pandas as pd
+
+repo_path = Path.cwd().parent.absolute()
+data_path = repo_path/'data'
+non_market_path = data_path/'non-market-housing.csv'
+
+
+def non_market_dataframe():
+    non_market_df = pd.read_csv(non_market_path, delimiter=';')
+
+    # Cleaning and Wrangling non_market_df
+    design_columns = {
+        column: 0 for column in non_market_df.columns if column.startswith('Design')
+        }
+    # Update design type
+    non_market_df.fillna(value=design_columns, inplace=True)
+    non_market_df.fillna(value={'Operator': 'not specified'}, inplace=True)
+
+    # Turn json/str to a dictionary to extract coordiantes
+    # Note the coordinates are written as [long, lat]
+    geometry_series = non_market_df['Geom'].apply(lambda x: json.loads(x))
+    non_market_df['longitude'] = geometry_series.apply(lambda x: x['coordinates'][0])
+    non_market_df['latitude'] = geometry_series.apply(lambda x: x['coordinates'][1])
+
+    # Add a column with the total number of units for each project
+    units_per_housing = non_market_df[list(design_columns.keys())].sum(axis=1)
+    non_market_df['Total Units'] = units_per_housing
+    return non_market_df
