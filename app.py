5b1467ff43b68247e3d42b807ddcbfb858eab390
Faisal Barazi <f.barazi@gmail.com>
2021-09-25 12:01:09 -0700

    Update the app to render more graphs and adjust the layout and text.

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from dash import html
 import plotly.express as px
 import os
 
-from etl_data import non_market_dataframe
+from etl_data import (
+    non_market_dataframe, design_type_dataframe,
+    clientele_type_dataframe)
 
 app = dash.Dash(__name__)
 
@@ -12,7 +14,9 @@ server = app.server
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
-df = non_market_dataframe()
+main_df = non_market_dataframe()
+design_type_df = design_type_dataframe()
+clientele_df = clientele_type_dataframe()
 
 # Update me. Properly access the required token for mapbox for deployement.
 token = os.environ['MAPBOX_TOKEN']
@@ -20,8 +24,10 @@ token = os.environ['MAPBOX_TOKEN']
 
 # TODO: Read the token form a text file. Easier
 px.set_mapbox_access_token(token)
+
+# All figures here.
 fig = px.scatter_mapbox(
-    df,
+    main_df,
     lat='latitude',
     lon='longitude',
     mapbox_style='carto-positron',
@@ -32,22 +38,86 @@ fig = px.scatter_mapbox(
     size='Total Units',
     zoom=10
 )
+bar_fig = px.bar(
+    design_type_df, x='Unit Type', y='Count',
+    color_discrete_sequence=['#a32f28'],
+)
 
-app.layout = html.Div(children=[
-    html.H1(children='Interactive map of Non Market Housing in Vancouver.'),
+pie_fig = px.pie(
+  clientele_df, values='Count', names='Client', color_discrete_sequence=px.colors.sequential.RdBu
+  )
+
+
+# layout here.
+app.layout = html.Div([
+    html.H1('Exploring the Non Market Housing dataset.'),
+
+    html.Div([
+        html.H2('General information'),
+        html.P([
+            'The  ',
+            html.A(
+                'dataset ',
+                href='https://opendata.vancouver.ca/explore/dataset/non-market-housing/information/'
+            ),
+
+            'is provided by the city of vancouver open data initiative. ',
+            'The data is provided under the ',
+            html.A('Open Government Licence.', href='https://opendata.vancouver.ca/pages/licence/'),
+            html.Br(),
+            '''
+            The dataset provided information on non market housing developed and under development
+            Between the year 1958 - 2020.
+            '''
+            ])
+        ]),
+
 
     # Interacttive Map division
     html.Div([
-        html.P(
-            'The map shows locations and information of '
-            'non market housing between the years 1958 - 2020.\n'
-            'The projects are color coded depending on their phase '
-            'and the scale reflects the capacity of the unit.'
-        ),
+        html.H2('Interactive map of the Non market Housing Dataset.'),
+        html.P([
+            '''The map shows locations and information of
+            non market housing between the years 1958 - 2020.''',
+            html.Br(),
+            '''The projects are color coded depending on their phase
+            and the scale reflects the capacity of the unit.''',
+            html.Br(),
+            '''
+            Hover over the circles to get more information about the project.
+            '''
+            ]),
         dcc.Graph(
             id='interactive-map',
-            figure=fig
-        )
+            figure=fig)
+    ]),
+    # Design type statistics.
+    html.Div([
+        html.H2('Unit counts for each Unit type.'),
+        html.P([
+            '''Here we look at the count of units per design type between 1958 - 2020.''',
+            html.Br(),
+            '''Note that we are only looking at completed and under construction projects in this stat.'''
+            ]),
+        dcc.Graph(
+            id='unit-type-stats',
+            figure=bar_fig)
+    ]),
+
+    # Pie chart by clientele
+    html.Div([
+        html.H2('Percentage of clientele 1958 and 2020'),
+        html.P([
+            '''Here we look at what percentage of the developments are for which clientele type.''',
+            html.Br(),
+            '''Note that here were looking as well at projects that were completed or under construction.''',
+            html.Br(),
+            '''\'Other\' here is a big umbrella and not will defined in the dataset.'''
+
+            ]),
+        dcc.Graph(
+            id='clientele-percentage-stats',
+            figure=pie_fig)
     ]),
 
 ])
